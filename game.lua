2e1fbe5dd820123a1c5a317951371f12ae45fe8a
Idriss <idriss.dimson@gmail.com>
2018-02-18 19:31:44 -0500

    Cleaning up
    cleaning up the app quickly

--- game.lua
+++ game.lua
@@ -13,17 +13,20 @@
 -- define widget
 local widget = require("widget")
 
-local json = require "json"
-
 local composer = require( "composer" )
 local scene = composer.newScene()
+
+-- Read/write files
+local GBCDataCab = require("plugin.GBCDataCabinet")
 
 local Btn1
 local Btn2
 local Btn3
 local Btn4
-
---json doesn't work on corona anymore WTF??!!
+local timeLeft
+local time = 0
+local timeLimit = 20
+local score = 0
 function scene:create( event )
 
 	-- Called when the scene's view does not exist.
@@ -33,21 +36,22 @@ function scene:create( event )
 
 	local sceneGroup = self.view
     
-    local timeText = display.newText( "TIME:", display.contentWidth/2.3, display.contentHeight/6, "Nunito-Black", 60 )
+    local timeText = display.newText( "TIME:", display.safeActualContentWidth/2.3, display.safeActualContentHeight/6, "Nunito-Black", 60 )
     timeText:setFillColor(.27, .651, 60)
-    local scoreText = display.newText( "SCORE:", display.contentWidth/2.3, display.contentHeight/4.5, "Nunito-Regular", 40 )
+    local scoreText = display.newText( "SCORE:", display.safeActualContentWidth/2.3, display.safeActualContentHeight/4.5, "Nunito-Black", 40 )
     scoreText:setFillColor(.27, .651, 60)
-    local numberText = display.newText( 0, display.contentCenterX, display.contentCenterY+45, "Nunito-Regular", 100 )
+    local numberText = display.newText( 0, display.safeActualContentWidth/2, display.safeActualContentHeight/1.8, "Nunito-Regular", 100 )
     numberText:setFillColor(0, 0, 0)
     numberText.text = ( math.random(1, 4) )
-
-    local scoreNumber = display.newText( 0, display.contentWidth/1.7, display.contentHeight/4.5, "Nunito-Regular", 60 )
+    timeLeft = display.newText(20, display.safeActualContentWidth/1.7, display.safeActualContentHeight/6, "Nunito-Black", 65)
+    timeLeft:setFillColor(.27, .651, 60)
+    local scoreNumber = display.newText( 0, display.safeActualContentWidth/1.7, display.safeActualContentHeight/4.5, "Nunito-Regular", 60 )
     scoreNumber:setFillColor(.27, .651, 60)
-
     local function check()
         if Btn1:getLabel() == numberText.text then
             print("You won Da prize boii!")
-            scoreNumber.text = scoreNumber.text + 1
+            score = score + 1
+            scoreNumber.text = score
             return true
         else
             print("sike!! that's ze wrong numero!")
@@ -57,7 +61,8 @@ function scene:create( event )
     local function check2()
         if Btn2:getLabel() == numberText.text then
             print("You won Da prize boii!")
-            scoreNumber.text = scoreNumber.text + 1
+            score = score + 1
+            scoreNumber.text = score
             return true
         else
             print("sike!! that's ze wrong numero!")
@@ -67,7 +72,8 @@ function scene:create( event )
     local function check3()
         if Btn3:getLabel() == numberText.text then
             print("You won Da prize boii!")
-            scoreNumber.text = scoreNumber.text + 1
+            score = score + 1
+            scoreNumber.text = score
             return true
         else
             print("sike!! that's ze wrong numero!")
@@ -77,7 +83,8 @@ function scene:create( event )
     local function check4()
         if Btn4:getLabel() == numberText.text then
             print("You won Da prize boii!")
-            scoreNumber.text = scoreNumber.text + 1
+            score = score + 1
+            scoreNumber.text = score
             return true
         else
             print("sike!! that's ze wrong numero!")
@@ -123,65 +130,84 @@ function scene:create( event )
         onRelease = onBtnRelease,
         --properties for a rectangle button...
         shape = "Rect",
-        width = display.contentWidth/2,
-        height = display.contentHeight/4,
+        width = display.safeActualContentWidth/2,
+        height = display.safeActualContentHeight/4,
         cornerRadius = 0,
         labelColor = { default = { 8/255, 19/255, 166/255, 1 }, over = { 149/255, 148/255, 209/255, 1} },
         fillColor = { default={ 149/255, 148/255, 209/255, 1 }, over={  8/255, 19/255, 166/255, 0.4 } },
-        fontSize = 70
-    }
+        fontSize = 70,
+        font = "Nunito-Regular"}
     Btn2 = widget.newButton {
         label = "2",
         onPress = check2,
         onRelease = onBtnRelease,
         --properties for a rectangle button...
         shape = "Rect",
-        width = display.contentWidth/2,
-        height = display.contentHeight/4,
+        width = display.safeActualContentWidth/2,
+        height = display.safeActualContentHeight/4,
         cornerRadius = 0,
         labelColor = { default = { 166/255, 8/255, 8/255, 1 }, over = { 207/255, 147/255, 147/255, 1} },
         fillColor = { default={ 207/255, 147/255, 147/255, 1 }, over={ 166/255, 8/255, 8/255, 0.4 } },
-        fontSize = 70
-    }
+        fontSize = 70,
+        font = "Nunito-Regular"}
     Btn3 = widget.newButton {
         label = "3",
         onPress = check3,
         onRelease = onBtnRelease,
         --properties for a rectangle button...
         shape = "Rect",
-        width = display.contentWidth/2,
-        height = display.contentHeight/4,
+        width = display.safeActualContentWidth/2,
+        height = display.safeActualContentHeight/4,
         cornerRadius = 0,
         labelColor = { default = { 166/255, 8/255, 8/255, 1 }, over = { 207/255, 147/255, 147/255, 1} },
         fillColor = { default={ 207/255, 147/255, 147/255, 1 }, over={ 166/255, 8/255, 8/255, 0.4 } },
-        fontSize = 70
-    }
+        fontSize = 70,
+        font = "Nunito-Regular"}
     Btn4 = widget.newButton {
         label = "4",
         onPress = check4,
         onRelease = onBtnRelease,
         --properties for a rectangle button...
         shape = "Rect",
-        width = display.contentWidth/2,
-        height = display.contentHeight/4,
+        width = display.safeActualContentWidth/2,
+        height = display.safeActualContentHeight/4,
         cornerRadius = 0,
         labelColor = { default = { 8/255, 19/255, 166/255, 1 }, over = { 149/255, 148/255, 209/255, 1} },
         fillColor = { default={ 149/255, 148/255, 209/255, 1 }, over={  8/255, 19/255, 166/255, 0.4 } },
-        fontSize = 70
-    }
-    local numberRect = display.newRoundedRect( display.contentCenterX, display.contentCenterY+50, display.contentWidth/4, display.contentHeight/7, 25 )
+        fontSize = 70,
+        font = "Nunito-Regular"}
+    local numberRect = display.newRoundedRect( display.safeActualContentWidth/2, display.safeScreenOriginY + (display.safeActualContentHeight/2), display.safeActualContentWidth/4, display.safeActualContentHeight/7, 25 )
     
-    Btn1.x = display.contentCenterX - (display.contentWidth/4);
-    Btn1.y = display.contentCenterY - (display.contentHeight/12);
+    Btn1.x = display.safeScreenOriginX + (display.safeActualContentWidth/4);
+    Btn1.y = display.safeScreenOriginY + (display.safeActualContentHeight/3);
     
-    Btn2.x = display.contentCenterX + (display.contentWidth/4);
-    Btn2.y = display.contentCenterY - (display.contentHeight/12);
+    Btn2.x = display.safeScreenOriginX + (display.safeActualContentWidth/1.32);
+    Btn2.y = display.safeScreenOriginY + (display.safeActualContentHeight/3);
     
-    Btn3.x = display.contentCenterX - (display.contentWidth/4);
-    Btn3.y = display.contentCenterY + (display.contentHeight/6);
+    Btn3.x = display.safeScreenOriginX + (display.safeActualContentWidth/4);
+    Btn3.y = display.safeScreenOriginY + (display.safeActualContentHeight/1.7);
     
-    Btn4.x = display.contentCenterX + (display.contentWidth/4);
-    Btn4.y = display.contentCenterY + (display.contentHeight/6);
+    Btn4.x = display.safeScreenOriginX + (display.safeActualContentWidth/1.32);
+    Btn4.y = display.safeScreenOriginY + (display.safeActualContentHeight/1.7);
+
+    -- does a cabinet "Gamescores" exists?
+    local CabExist = GBCDataCab.load("Gamescores")
+
+    -- if cabinet does not exist, then create it.
+    if CabExist == false then
+        CabExist = GBCDataCab.createCabinet("Gamescores")
+    end
+
+    -- if the cabinet does exist, or if it was just created, try to extract some data    
+    if CabExist then
+        values = GBCDataCab.get("Gamescores", "Player Scores")
+
+        -- if values is not nil, then you have data.  Use it.
+    else
+        -- if values is nil, then you do not have any data.  
+        -- Probably because this is the first time running the app.
+        -- initialize your variables here
+    end
     
     sceneGroup:insert( timeText )
     sceneGroup:insert( scoreText )
@@ -192,6 +218,7 @@ function scene:create( event )
     sceneGroup:insert( Btn4 )
     sceneGroup:insert( numberRect )
     sceneGroup:insert( numberText )
+    sceneGroup:insert( timeLeft )
 end
 
 
@@ -201,15 +228,13 @@ function scene:show( event )
 	
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
+    score = 0
+    timeLeft:setFillColor(.27, .651, 60)
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
 		-- 
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
-        
-        local timeLimit = 20
-        timeLeft = display.newText(timeLimit, display.contentWidth/1.7, display.contentHeight/6, native.systemFontBold, 65)
-        timeLeft:setFillColor(.27, .651, 60)
 
         local function timerDown()
            timeLimit = timeLimit-1
@@ -219,13 +244,12 @@ function scene:show( event )
                 timeLeft:setFillColor(255,0,0)
             elseif(timeLimit==0)then
                 print("Time Out") -- or do your code for time out
+                timeLimit = 20
                 composer.gotoScene( "gameover", "fade", 250 )
             end
           end
         timer.performWithDelay(1000,timerDown,timeLimit)
         
-        
-        sceneGroup:insert( timeLeft )
 	end
 end
 
@@ -239,10 +263,10 @@ function scene:hide( event )
 		--
 		-- INSERT code here to pause the scene
 		-- e.g. stop timers, stop animation, unload sounds, etc.)
+        timeLimit = 20
 	elseif phase == "did" then
-		-- Called when the scene is now off screen
-	end	
-	
+        -- Called when the scene is now off screen
+    end 
 end
 
 function scene:destroy( event )
